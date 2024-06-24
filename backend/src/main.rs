ea695e525b4191a756e887601d7fb3eb7c1d9daf
David Mo <44306450+unknown@users.noreply.github.com>
2024-06-24 19:29:05 -0400

    Create Axum server

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -3,23 +3,51 @@ mod jobs;
 
 use std::time::Duration;
 
+use axum::{extract, http::StatusCode, routing::post, Json, Router};
 use jobs::{Job, RustJob};
 use nomad_rs::Nomad;
+use serde::{Deserialize, Serialize};
 
 #[tokio::main]
 async fn main() {
+    let app = Router::new().route("/execute-rust", post(execute_rust));
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+#[derive(Debug, Deserialize)]
+struct ExecuteRustRequest {
+    code: String,
+}
+
+#[derive(Debug, Serialize)]
+#[serde(tag = "status")]
+enum ExecuteRustResponse {
+    Success { output: String },
+    Error { error: String },
+}
+
+async fn execute_rust(
+    extract::Json(payload): extract::Json<ExecuteRustRequest>,
+) -> (StatusCode, Json<ExecuteRustResponse>) {
     let nomad = Nomad::default();
     let timeout = Duration::from_secs(20);
     let interval = Duration::from_secs(1);
 
-    let code = r#"fn main() {
-    println!("Hello, from Rust code!");
-}"#;
-
-    let job = RustJob::new(code);
-
+    let job = RustJob::new(&payload.code);
     match job.execute(&nomad, timeout, interval).await {
-        Ok(output) => println!("Output:\n{}", output),
-        Err(error) => eprintln!("{}", error),
-    };
+        Ok(output) => (
+            StatusCode::OK,
+            Json(ExecuteRustResponse::Success { output }),
+        ),
+        Err(error) => (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            Json(ExecuteRustResponse::Error {
+                error: error.to_string(),
+            }),
+        ),
+    }
 }
